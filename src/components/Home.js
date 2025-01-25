50eeac2655a75144635d453eaf3cedb845a79b39
Mamta Patil <mamtapatil44@gmail.com>
2025-01-25 16:15:48 -0500

    image changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
         </div>
         <div className=" w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Profile Pic"/>
+            <img  className="rounded-2xl" src={profilePic} alt="Profile Pic"/>
           </div>
         </div>
       </div>
